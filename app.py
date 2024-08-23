aababa2ad9918b64216e420fbfa6d923cca897d8
JanisaMalaviya <janisamalaviya@hotmail.com>
2024-08-23 22:44:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -53,8 +53,12 @@ except Exception as ex:
         premature_stop = True
         handle_uncaught_app_exception(ex)
         uncaught_exception = ex
-
-  return(result, run_without_errors, rerun_exception_data, premature_stop, uncaught_exception)
+ return(result, 
+        run_without_errors, 
+        rerun_exception_data, 
+        premature_stop, 
+        uncaught_exception
+       )
 
 movies= pickle.load(open("artifacts\movie_list.pkl","rb" ))
 similarity= pickle.load(open("artifacts\similarity.pkl","rb"))
